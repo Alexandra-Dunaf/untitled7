37dcdbed742353bd19ac9f50ebda89c2b3e05feb
Sanya <sashaklgn@gmail.com>
2021-12-23 15:51:14 +0400

    Initial commit

---
+++ src/MyClass.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class MyClass {
+    Integer[] myArray;
+
+    public MyClass(Integer[] myArray) {
+        this.myArray = myArray;
+    }
+
+    public void print() {
+        System.out.println(Arrays.toString(myArray));
+    }
+
+    public Integer[] getMyArray() {
+        return myArray;
+    }
+}
